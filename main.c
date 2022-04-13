4601f91afac91f74e2e13fa3120766bd6b79bfab
sebarraza2-web <98637494+sebarraza2-web@users.noreply.github.com>
2022-04-12 22:12:17 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,36 +1,23 @@
 #include "msp.h"
-#include <lcdLib.h>
+#include "lcdLib.h"
 #include "keypad.h"
+#include "uart.h"
+#include "timers.h"
 
 char buffer[1];
 
-void uart_init(void){
-    P3->SEL0 |= BIT2 | BIT3;
-    EUSCI_A2->CTLW0 |= EUSCI_A_CTLW0_SWRST;
-    EUSCI_A2->CTLW0  = 0x00C1;
-    EUSCI_A2->BRW = 312;
-    EUSCI_A2->MCTLW &= ~0xFFF1;
-    EUSCI_A2->CTLW0 &= ~(EUSCI_A_CTLW0_SWRST);
-}
-void SysTick_timer(double x){
-    SysTick->CTRL = 5;
-    SysTick->VAL = 0;
-    SysTick->LOAD = (x*3000000)-1;
-    while(!(SysTick->CTRL & 0x00010000));
-}
 void transmit(void){
-    //int i;
-    //for(i = 0; i <= sizeof(buffer); i++){
+    int i;
+    for(i = 0; i <= sizeof(buffer); i++){
         SysTick_timer(1);
         if(EUSCI_A2->IFG & EUSCI_A_IFG_TXIFG){
             EUSCI_A2->TXBUF = buffer[0];
         }
-   // }
+    }
 }
 
 void main(void){
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;
-    //uart_init();
     keypad_init();
     lcdInit();
     lcdClear();
