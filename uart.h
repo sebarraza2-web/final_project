9e97a07b326e1b3ace49b54ba4bd3431344a87fa
sebarraza2-web <98637494+sebarraza2-web@users.noreply.github.com>
2022-04-12 22:10:51 -0600

    Create uart.h

---
+++ uart.h
@@ -0,0 +1,16 @@
+/*
+ * uart.h
+ *
+ *  Created on: Apr 12, 2022
+ *      Author: sebel
+ */
+
+#ifndef UART_H_
+#define UART_H_
+
+
+void uart_init();
+void transmit_string();
+
+
+#endif /* UART_H_ */
