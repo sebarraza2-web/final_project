f5e2cde460aebb6475f76061759b786a1d09648f
Chente71 <103529907+Chente71@users.noreply.github.com>
2022-04-14 11:26:43 -0600

    Update PWM.c

--- PWM.c
+++ PWM.c
@@ -1,7 +1,4 @@
 void PWM(void){
-        P2->DIR |= BIT4; // P2.4 set TA0.1
-        P2->SEL0 |= BIT4;
-        P2->SEL1 &= ~(BIT4);
         TIMER_A0->CCR[0] = 6900 - 1;                // PWM Period
         TIMER_A0->CCTL[1] = TIMER_A_CCTLN_OUTMOD_7; // CCR1 reset/set
         TIMER_A0->CCR[1] = 1000; // CCR1 PWM duty cycle
