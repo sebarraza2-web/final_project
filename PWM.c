32f1740f145d0a01ff86cd7fc978a4a4a60d21f7
Chente71 <103529907+Chente71@users.noreply.github.com>
2022-04-14 11:48:27 -0600

    Update PWM.c

--- PWM.c
+++ PWM.c
@@ -1,10 +1,10 @@
 void PWM(void){
-        TIMER_A0->CCR[0] = 6900 - 1;                // PWM Period
-        TIMER_A0->CCTL[1] = TIMER_A_CCTLN_OUTMOD_7; // CCR1 reset/set
-        TIMER_A0->CCR[1] = 1000; // CCR1 PWM duty cycle
-        TIMER_A0->CTL = TIMER_A_CTL_SSEL__SMCLK |  // SMCLK
-                TIMER_A_CTL_MC__UP | 
-                TIMER_A_CTL_CLR; 
-        SysTick_timer(3.596);                      // Delay for motor run time
-        TIMER_A0->CCR[1] = 0;                     // CCR1 PWM duty cycle
+        TIMER_A2->CCR[0] = 6900 - 1; // PWM Period
+        TIMER_A2->CCTL[4] = TIMER_A_CCTLN_OUTMOD_7; // CCR4 reset/set
+        TIMER_A2->CCR[4] = 1000; // CCR4 PWM duty cycle
+        TIMER_A2->CTL = TIMER_A_CTL_SSEL__SMCLK | // SMCLK
+                TIMER_A_CTL_MC__UP | // Up mode
+                TIMER_A_CTL_CLR; // Clear TAR
+        SysTick_timer(3.6);
+        TIMER_A2->CCR[4] = 0; 
 }
