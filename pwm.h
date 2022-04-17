6950c5bd5d6c8fa4e05166e3c01cc0aa166e98eb
sebarraza2-web <98637494+sebarraza2-web@users.noreply.github.com>
2022-04-17 04:34:53 -0600

    Create pwm.h

---
+++ pwm.h
@@ -0,0 +1,16 @@
+/*
+ * pwm.h
+ *
+ *  Created on: Apr 17, 2022
+ *      Author: sebastian barraza
+ */
+
+#ifndef PWM_H_
+#define PWM_H_
+
+void PWM_Init(void);
+void PWM(void);
+
+
+
+#endif /* PWM_H_ */
