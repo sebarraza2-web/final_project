a5c5e030111acbb729c7424f489d338702ba0bc3
sebarraza2-web <98637494+sebarraza2-web@users.noreply.github.com>
2022-05-05 00:10:31 -0600

    Update uart.c

--- uart.c
+++ uart.c
@@ -10,18 +10,18 @@
 
 
 void uart_init(void){
-    P3->SEL0 |= BIT2 | BIT3;
-    EUSCI_A2->CTLW0 |= EUSCI_A_CTLW0_SWRST;
-    EUSCI_A2->CTLW0  = 0x00C1;
-    EUSCI_A2->BRW = 312;
-    EUSCI_A2->MCTLW &= ~0xFFF1;
-    EUSCI_A2->CTLW0 &= ~(EUSCI_A_CTLW0_SWRST);
+    P3->SEL0 |= BIT2 | BIT3; // Selected pins for UCA_2
+    EUSCI_A2->CTLW0 |= EUSCI_A_CTLW0_SWRST; // Sets the reset bit to allow configuration
+    EUSCI_A2->CTLW0  = 0x00C1;              // Uses SMCLK which runs @ 3MHz
+    EUSCI_A2->BRW = (3000000) / (9600); // Baud Rate Calculation used 9600 which is a standard
+    EUSCI_A2->MCTLW &= ~0xFFF1;         // No oversampling needed
+    EUSCI_A2->CTLW0 &= ~(EUSCI_A_CTLW0_SWRST); // Bit Clears the reset to set new configuration
 }
 
-void transmit_string(char *str){
-    while(*str != 0){
-        while(!(EUSCI_A2->IFG & EUSCI_A_IFG_TXIFG));
-        EUSCI_A2->TXBUF = *str++;
+void transmit_string(char *str){   // argument as a string
+    while(*str != 0){               // will tranmsit as long as the string has data
+        while(!(EUSCI_A2->IFG & EUSCI_A_IFG_TXIFG)); // waits for the TX buffer to be empty
+        EUSCI_A2->TXBUF = *str++;                   // sends corresponding character
     }
 }
 
