d9ac8386fe70d96849eae1dbe6b47f9830a86a90
sebarraza2-web <98637494+sebarraza2-web@users.noreply.github.com>
2022-05-05 00:04:19 -0600

    Update timers.c

--- timers.c
+++ timers.c
@@ -8,11 +8,11 @@
 #include "timers.h"
 
 
-void SysTick_timer(double x){
-    SysTick->CTRL = 5;
-    SysTick->VAL = 0;
-    SysTick->LOAD = (x*3000000)-1;
-    while(!(SysTick->CTRL & 0x00010000));
+void SysTick_timer(double x){ // Timer used for delays
+    SysTick->CTRL = 5; // no interrupts 
+    SysTick->VAL = 0; // dummy load value
+    SysTick->LOAD = (x*3000000)-1; // takes the argument and does calculation
+    while(!(SysTick->CTRL & 0x00010000)); // will stay in this statement until the time is reached
 }
 
 
