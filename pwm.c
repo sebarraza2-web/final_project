0f3d8545c4f90546b84770e06d4d7ba08b5415e4
sebarraza2-web <98637494+sebarraza2-web@users.noreply.github.com>
2022-05-05 00:20:12 -0600

    Update pwm.c

--- pwm.c
+++ pwm.c
@@ -12,27 +12,23 @@
 void PWM_Init(void){
        P5->DIR |= BIT7; // P5.7 set TA2.2
        P5->SEL0 |= BIT7;
+       
+       TIMER_A2->CCR[0] = 6900 - 1; // PWM Period
+       TIMER_A2->CCTL[2] = TIMER_A_CCTLN_OUTMOD_7; // CCR4 reset/set
+       TIMER_A2->CTL = TIMER_A_CTL_SSEL__SMCLK | // SMCLK
+                TIMER_A_CTL_MC__UP | // Up mode
+                TIMER_A_CTL_CLR; // Clear TAR
 }
 
 
 void PWM_reset(void){
-        TIMER_A2->CCR[0] = 6900 - 1; // PWM Period
-        TIMER_A2->CCTL[2] = TIMER_A_CCTLN_OUTMOD_7; // CCR4 reset/set
-        TIMER_A2->CCR[2] = 1000; // CCR4 PWM duty cycle
-        TIMER_A2->CTL = TIMER_A_CTL_SSEL__SMCLK | // SMCLK
-                TIMER_A_CTL_MC__UP | // Up mode
-                TIMER_A_CTL_CLR; // Clear TAR
-        SysTick_timer(1);
+      TIMER_A2->CCR[2] = 1000; // CCR4 PWM duty cycle
+      SysTick_timer(1);
 }
 
 void PWM_forward(void){
-        TIMER_A2->CCR[0] = 6900 - 1; // PWM Period
-        TIMER_A2->CCTL[2] = TIMER_A_CCTLN_OUTMOD_7; // CCR4 reset/set
-        TIMER_A2->CCR[2] = 6800; // CCR4 PWM duty cycle
-        TIMER_A2->CTL = TIMER_A_CTL_SSEL__SMCLK | // SMCLK
-                TIMER_A_CTL_MC__UP | // Up mode
-                TIMER_A_CTL_CLR; // Clear TAR
-        SysTick_timer(1);
+      TIMER_A2->CCR[2] = 6800; // CCR4 PWM duty cycle
+      SysTick_timer(1);
 }
 
 
